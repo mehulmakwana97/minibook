b241b744633d11b6584b5caa208a4d1f917e7a21
Makwana Mehul <mehulmakwana97@gmail.com>
2016-12-27 18:19:24 +0530

    Updated app.component.html

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `
-    <h1>Hello {{name}}</h1>
-    <nav>
-      <a routerLink="/" routerLinkActive="active">Home</a>
-      <a routerLink="/page-1" routerLinkActive="active">Page1</a>
-      <a routerLink="/page-2" routerLinkActive="active">Page2</a>
-    </nav>
-    <router-outlet></router-outlet>
-   `,
+  templateUrl: 'app.component.html'
 })
 export class AppComponent  { name = 'Angular test 1'; }
